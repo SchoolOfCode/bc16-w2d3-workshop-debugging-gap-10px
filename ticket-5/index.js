4bbd2e75c384d18a8e7f0b4721fbed036f27bf32
Gavin P <gavinapersad@gmail.com>
2024-02-14 13:48:53 +0000

    ticket 5 complete

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -6,7 +6,6 @@ const shoppingCart = [
   },
   {
     product: "Apples",
-
     price: 0.7,
     quantity: 8,
   },
@@ -19,11 +18,12 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
+for (let i = 0; i < shoppingCart.length; i++) {
   const item = shoppingCart[i];
   const costForItem = item.price * item.quantity;
   totalCost += costForItem;
 }
+
 
 const formatter = new Intl.NumberFormat("en-GB", {
   style: "currency",
